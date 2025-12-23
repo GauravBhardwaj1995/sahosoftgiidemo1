5599a469b6cfcfc3b7a90182e6a782156e2dd7b7
Gaurav <bhardwajgaurav.sre@gmail.com>
2025-12-23 16:11:21 +0530

    hello.js file added.

---
+++ hello.js
@@ -0,0 +1,11 @@
+Pakistan remained extremely crucial factor in Indo-US relations during the Cold War era. However, the
+effect of Pak factor in US-India relations seems to be waning, as it is no longer as effective as it used to be during
+the Cold War period. However, it does not mean that Pakistan has lost its importance as a factor in Indo-US
+relations. The issue of F-16 fighter aircrafts that the US was willing to supply to Pakistan and the India‟s objection
+to the deal can be cited as one of the latest examples that Pakistan as a factor in Indo-US relations cannot be
+completely ignored. The competition and hostilities between the communist bloc led by Soviet Union and the
+capitalist bloc headed by the US on the one hand and India‟s vital and firm support for non-alignment, along with its
+inclination towards the former due to several factors, made Pakistan closer to the U.S. and “From India‟s point of
+view the chief problem was the determined and persistent attempts by Washington to create an artificial parity
+between India and Pakistan. This quest had combined with repeated arms aid to Pakistan, some of which was at
+times meant to put pressure on India.”1
